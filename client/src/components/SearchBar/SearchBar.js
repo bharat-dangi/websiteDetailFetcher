fd76283da58b02a734623186e12fb27d51558e36
Bharat Dangi <bharatdangi2074@gmail.com>
2021-10-06 15:58:30 +0545

    Added API cal from frontend to backend

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./searchbar.css";
+import axios from "axios";
 
-const SearchBar = () => {
+const SearchBar = ({ setDomainDetails }) => {
+  const [domain, setDomain] = useState("");
+
+  const handleChange = (event) => {
+    setDomain(event.target.value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.get(
+      `http://localhost:8000/api/domain/${domain}`
+    );
+    setDomainDetails(data);
+  };
+
   return (
     <div className="searchBarContainer">
       <div className="searchbar">
-        <input placeholder="Type Website Name " className="searchInput" />
+        <input
+          placeholder="Type Website Name "
+          className="searchInput"
+          onChange={handleChange}
+          type="text"
+          value={domain}
+        />
       </div>
       <div>
-        <button>FETCH INFO</button>
+        <button onClick={onSubmit}>FETCH INFO</button>
       </div>
     </div>
   );
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Info from "../../components/Info/Info";
 import NavBar from "../../components/NavBar/NavBar";
 import SearchBar from "../../components/SearchBar/SearchBar";
 
 const Home = () => {
+  const [domainDetails, setDomainDetails] = useState(null);
+  console.log("DETAIL: ", domainDetails);
+
   return (
     <div>
       <NavBar />
-      <SearchBar />
-      <Info/>
+      <SearchBar setDomainDetails={setDomainDetails} />
+      <Info />
     </div>
   );
 };
